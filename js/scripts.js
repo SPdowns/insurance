99b5486b91d37ce5a294381b940e990e505a392e
Sean Downs <spdowns@protonmail.com>
2020-03-26 13:11:46 -0700

    first step

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("form#insurance").submit(function(event) {
+    var age = parseInt($("input#age").val());
+    var gender = $("select#gender").val();
+
+    var quote = (100 - age) * 3;
+
+    $("#rate").empty().append(quote);
+    $("#quote").show();
+  } else {
+    alert('Please enter your age');
+    
+    event.preventDefault();
+  });
+});
